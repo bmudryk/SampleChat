c02f2dc59ac39547f9461d70e5d9c0c3eda9cf78
bmudryk <bmudryk@gmail.com>
2019-03-09 15:50:24 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
-
+const http = require('http');
 const port=process.env.PORT || 3000
-var express = require('express');
-var bodyParser = require('body-parser')
-var http = require('http');
-var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.post('/messages', (req, res) => {
-    try {
-        io.emit('message', req.body);
-        res.sendStatus(200);
-    }
-    catch (error) {
-        res.sendStatus(500);
-        return console.log('Error:  ', error);
-    }
-    finally {
-        console.log('Message Posted')
-    }
-
-})
-
-io.on('connection', () => {
-    console.log('A person has connected')
-})
-
-server.listen(port, () => {
-    console.log('Server is running on port:  ', server.address().port);
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
 });
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
+
+
+// const port=process.env.PORT || 3000
+// var express = require('express');
+// var bodyParser = require('body-parser')
+// var http = require('http');
+// var app = express();
+// var server = http.createServer(app);
+// var io = require('socket.io').listen(server);
+
+// app.use(express.static(__dirname));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// app.post('/messages', (req, res) => {
+//     try {
+//         io.emit('message', req.body);
+//         res.sendStatus(200);
+//     }
+//     catch (error) {
+//         res.sendStatus(500);
+//         return console.log('Error:  ', error);
+//     }
+//     finally {
+//         console.log('Message Posted')
+//     }
+
+// })
+
+// io.on('connection', () => {
+//     console.log('A person has connected')
+// })
+
+// server.listen(port, () => {
+//     console.log('Server is running on port:  ', server.address().port);
+// });
