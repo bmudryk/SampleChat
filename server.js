126549e34a0ecdd8259dccaf816acd91847ec3db
bmudryk <bmudryk@gmail.com>
2019-03-09 11:50:25 -0500

    adding initial files

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/messages', (req, res) => {
+    try {
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    }
+    catch (error) {
+        res.sendStatus(500);
+        return console.log('Error:  ', error);
+    }
+    finally {
+        console.log('Message Posted')
+    }
+
+})
+
+io.on('connection', () => {
+    console.log('A person has connected')
+})
+
+var server = http.listen(3000, () => {
+    console.log('Server is running on port:  ', server.address().port);
+});
