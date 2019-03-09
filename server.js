6a018b501a301fb6626cf693b45ec9fcefe5586b
bmudryk <bmudryk@gmail.com>
2019-03-09 17:20:08 -0500

    adding back

--- server.js
+++ server.js
@@ -13,24 +13,24 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// app.post('/messages', (req, res) => {
-//     try {
-//         io.emit('message', req.body);
-//         res.sendStatus(200);
-//     }
-//     catch (error) {
-//         res.sendStatus(500);
-//         return console.log('Error:  ', error);
-//     }
-//     finally {
-//         console.log('Message Posted')
-//     }
+app.post('/messages', (req, res) => {
+    try {
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    }
+    catch (error) {
+        res.sendStatus(500);
+        return console.log('Error:  ', error);
+    }
+    finally {
+        console.log('Message Posted')
+    }
 
-// })
+})
 
-// io.on('connection', () => {
-//     console.log('A person has connected')
-// })
+io.on('connection', () => {
+    console.log('A person has connected')
+})
 
 server.listen(port, () => {
     console.log('Server is running on port:  ', server.address().port);
